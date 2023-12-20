de5db0121297103de5be9a74ffdd31502e93128c
nicksof <72809009+nicksof@users.noreply.github.com>
2023-12-20 08:38:22 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.crtm2fzh1kjt.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "nick-employee"
+customregion = "us-east-1"
+
